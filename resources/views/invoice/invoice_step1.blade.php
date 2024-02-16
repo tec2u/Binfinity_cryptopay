16ed38e37ed622f539a42a756b0ec3a4a3be132d
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-02-16 14:11:44 -0300

    add favicon invoice

--- resources/views/invoice/invoice_step1.blade.php
+++ resources/views/invoice/invoice_step1.blade.php
@@ -16,6 +16,7 @@
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link rel="icon" type="image/png" sizes="400x400" href="assetsWelcomeNew/images/icon.png">
 </head>
 
 <body>
