dac6203e68a6679d49dcc78424f29d34cee18696
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-02-19 12:02:26 -0300

    ajuste headers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
       \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
       \App\Http\Middleware\TrimStrings::class,
       \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+      \App\Http\Middleware\SecurityHeaders::class,
    ];
 
    /**
@@ -38,7 +39,6 @@ class Kernel extends HttpKernel
          \Illuminate\Routing\Middleware\SubstituteBindings::class,
          \RealRashid\SweetAlert\ToSweetAlert::class,
          \App\Http\Middleware\Locale::class,
-         \App\Http\Middleware\SecurityHeaders::class,
       ],
 
       'api' => [
