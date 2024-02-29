e17929de65a69c4a0773c76c2450e60c299347bb
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-02-29 12:04:49 -0300

    liberar atualizacao carteira

--- public/transactions/config.php
+++ public/transactions/config.php
@@ -1,14 +1,14 @@
 <?php
 
-$banco_user='binfinit_cryptopay';
-$banco_senha='Hnash@kjPbab@';
-$con =  @mysqli_connect("localhost", $banco_user, $banco_senha,"binfinit_cryptopay");
+$banco_user = 'root';
+$banco_senha = 'root';
+$con = @mysqli_connect("localhost", $banco_user, $banco_senha, "binfinit_cryptopay");
 
 
-@mysqli_query($con,"SET NAMES 'utf8'");
-@mysqli_query($con,'SET character_set_connection=utf8');
-@mysqli_query($con,'SET character_set_client=utf8');
-@mysqli_query($con,'SET character_set_results=utf8');	
+@mysqli_query($con, "SET NAMES 'utf8'");
+@mysqli_query($con, 'SET character_set_connection=utf8');
+@mysqli_query($con, 'SET character_set_client=utf8');
+@mysqli_query($con, 'SET character_set_results=utf8');
 
 
 
--- public/transactions/index.php
+++ public/transactions/index.php
@@ -1,53 +1,190 @@
-<?php
-error_reporting(E_ERROR | E_PARSE);
-include "config.php";
+<!DOCTYPE html>
+<html lang="en">
 
-if($_GET['type']=='' or $_GET['type']=='1'){$and = 'and type=1';}else{$and = 'and type=2';}
+<head>
 
-$sql = mysqli_query($con,"select * from node_orders where id>83 $and order by id desc");
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="B Inifnity Bank - Crypto Pay">
+		<meta name="author" content="BInfinity">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+		<link rel="icon" type="image/png" sizes="400x400" href="../assetsWelcomeNew/images/icon.png">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+			integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<title>B Inifnity C - Relatorio</title>
+		<style>
+			tr td {
+				overflow-x: auto;
+				max-width: 100px;
+			}
 
-if($_GET['change_withdrawal']!=''){
-	
-	
-	$get_change = mysqli_fetch_array(mysqli_query($con,"select * from node_orders where id=".$_GET['id']));
-	
-	if($get_change['withdrawn']==1){$change=0;}
-	if($get_change['withdrawn']==0){$change=1;}
-	
-	mysqli_query($con,"update node_orders set withdrawn=$change where id=".$_GET['id']);
-	
-}
+			tbody tr {
+				margin-top: 2rem;
+			}
+		</style>
+	</head>
+</head>
 
-echo "<a href='?type=1'>ENTRADAS</a> | <a href='?type=2'>SAIDAS</a></br></br>";
+<body>
+	<?php
 
-while($pega_order = mysqli_fetch_array($sql)){
-	
-	if($pega_order['status']=="failed"){$color='red';}
-	if($pega_order['status']=="Expired"){$color='orange';}
-	if($pega_order['status']=="Paid"){$color='green';}
-	if($pega_order['status']=="Pending"){$color='yellow';}
-	
-	echo "--------------------------------------------------------------------------</br>";
-	echo "--------------------------------------------------------------------------</br>";
-	echo "<span style='background-color:$color'>$pega_order[id]|$pega_order[coin]|$pega_order[status]|$pega_order[price]</span> </br>";
-	
-	if ($pega_order['coin'] == 'TRX' or  $pega_order['coin'] == 'USDT_TRC20'){echo "<a href='https://tronscan.org/#/transaction/$pega_order[hash]'>LINK</a>";}
-	if ($pega_order['coin'] == 'ETH' or  $pega_order['coin'] == 'USDT_ERC20'){echo "<a href='https://etherscan.io/tx/$pega_order[hash]'>LINK</a>";}
-	if ($pega_order['coin'] == 'BTC'){echo "<a href='https://blockchair.com/bitcoin/transaction/$pega_order[hash]'>LINK</a>";}
-	
-	echo  "</br>CRYPTO PRICE: $pega_order[price_crypto] </br>
+	error_reporting(E_ERROR | E_PARSE);
+	include "config.php";
+
+	if ($_GET['type'] == '' or $_GET['type'] == '1') {
+		$and = 'and type=1';
+	} else {
+		$and = 'and type=2';
+	}
+
+	$sql = mysqli_query($con, "select * from node_orders where id>0 $and order by id desc");
+
+	if ($_GET['change_withdrawal'] != '') {
+
+
+		$get_change = mysqli_fetch_array(mysqli_query($con, "select * from node_orders where id=" . $_GET['id']));
+
+		if ($get_change['withdrawn'] == 1) {
+			$change = 0;
+		}
+		if ($get_change['withdrawn'] == 0) {
+			$change = 1;
+		}
+
+		mysqli_query($con, "update node_orders set withdrawn=$change where id=" . $_GET['id']);
+
+	}
+
+	echo "<a href='?type=1'>ENTRADAS</a> | <a href='?type=2'>SAIDAS</a></br></br>";
+
+	?>
+
+	<table class="table" style="overflow-x: scroll;">
+		<thead>
+			<tr>
+				<th scope="col">id</th>
+				<th scope="col">Coin</th>
+				<th scope="col">Status</th>
+				<th scope="col">Price</th>
+				<th scope="col">Price crypto</th>
+				<th scope="col">Wallet</th>
+				<th scope="col">Withdrawn</th>
+				<th scope="col">Change Withdrawn</th>
+				<th scope="col">Link Hash</th>
+			</tr>
+		</thead>
+		<tbody>
+
+			<?php while ($pega_order = mysqli_fetch_array($sql)): ?>
+				<?php
+				if (strtolower($pega_order['status']) == "failed") {
+					$class = 'table-danger';
+				}
+				if (strtolower($pega_order['status']) == "expired") {
+					$class = 'table-warning';
+				}
+				if (strtolower($pega_order['status']) == "paid") {
+					$class = 'table-success';
+				}
+				if (strtolower($pega_order['status']) == "pending") {
+					$class = 'table-light';
+				}
+
+				if ($pega_order['withdrawn'] == 1) {
+					$change = "No";
+				}
+				if ($pega_order['withdrawn'] == 0) {
+					$change = "Yes";
+				}
+
+				?>
+				<tr class="<?php echo $class ?>">
+					<td scope="col">
+						<?php echo $pega_order['id'] ?>
+					</td>
+					<td>
+						<?php echo $pega_order['coin'] ?>
+					</td>
+					<td>
+						<?php echo $pega_order['status'] ?>
+					</td>
+					<td>
+						<?php echo $pega_order['price'] * 1 ?>
+					</td>
+					<td>
+						<?php echo $pega_order['price_crypto'] * 1 ?>
+					</td>
+					<td>
+						<?php echo $pega_order['wallet'] ?>
+					</td>
+					<td>
+						<?php echo $pega_order['withdrawn'] == 1 ? "Yes" : "No" ?>
+					</td>
+					<td><a href='?change_withdrawal=1&id=$pega_order[id]'>CHANGE WITHDRAWAL TO <> $change</a></td>
+					<td>Link Hash</td>
+				</tr>
+
+			<?php endwhile; ?>
+		</tbody>
+	</table>
+
+
+	<?php
+
+	while ($pega_order = mysqli_fetch_array($sql)) {
+
+		if ($pega_order['status'] == "failed") {
+			$color = 'red';
+		}
+		if ($pega_order['status'] == "Expired") {
+			$color = 'orange';
+		}
+		if ($pega_order['status'] == "Paid") {
+			$color = 'green';
+		}
+		if ($pega_order['status'] == "Pending") {
+			$color = 'yellow';
+		}
+
+		echo "--------------------------------------------------------------------------</br>";
+		echo "--------------------------------------------------------------------------</br>";
+		echo "<span style='background-color:$color'>$pega_order[id]|$pega_order[coin]|$pega_order[status]|$pega_order[price]</span> </br>";
+
+		if ($pega_order['coin'] == 'TRX' or $pega_order['coin'] == 'USDT_TRC20') {
+			echo "<a href='https://tronscan.org/#/transaction/$pega_order[hash]'>LINK</a>";
+		}
+		if ($pega_order['coin'] == 'ETH' or $pega_order['coin'] == 'USDT_ERC20') {
+			echo "<a href='https://etherscan.io/tx/$pega_order[hash]'>LINK</a>";
+		}
+		if ($pega_order['coin'] == 'BTC') {
+			echo "<a href='https://blockchair.com/bitcoin/transaction/$pega_order[hash]'>LINK</a>";
+		}
+
+		echo "</br>CRYPTO PRICE: $pega_order[price_crypto] </br>
 		  CRYPTO PRICE PAID: $pega_order[price_crypto_payed] </br>
 		  
 		  WALLET: $pega_order[wallet]</br>
 		  WITHDRAWAL: $pega_order[withdrawn] </br>";
-		  
-		  if($pega_order['withdrawn']==1){$change="Zero";}
-		  if($pega_order['withdrawn']==0){$change="One";}
-		  
-		  echo "<a href='?change_withdrawal=1&id=$pega_order[id]'>CHANGE WITHDRAWAL TO $change</a></br>";
-	echo "--------------------------------------------------------------------------</br>";
-	echo "--------------------------------------------------------------------------</br>";
-	
-}
 
-?>
+		if ($pega_order['withdrawn'] == 1) {
+			$change = "Zero";
+		}
+		if ($pega_order['withdrawn'] == 0) {
+			$change = "One";
+		}
+
+		echo "<a href='?change_withdrawal=1&id=$pega_order[id]'>CHANGE WITHDRAWAL TO $change</a></br>";
+		echo "--------------------------------------------------------------------------</br>";
+		echo "--------------------------------------------------------------------------</br>";
+
+	}
+
+	?>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
+</body>
+
+</html>
