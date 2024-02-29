73793f8f142b2dd8b633e7b045e62382088fc432
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-02-29 14:53:44 -0300

    .

--- public/transactions/index.php
+++ public/transactions/index.php
@@ -181,54 +181,54 @@
 
 	<?php
 
-	while ($pega_order = mysqli_fetch_array($sql)) {
-
-		if ($pega_order['status'] == "failed") {
-			$color = 'red';
-		}
-		if ($pega_order['status'] == "Expired") {
-			$color = 'orange';
-		}
-		if ($pega_order['status'] == "Paid") {
-			$color = 'green';
-		}
-		if ($pega_order['status'] == "Pending") {
-			$color = 'yellow';
-		}
-
-		echo "--------------------------------------------------------------------------</br>";
-		echo "--------------------------------------------------------------------------</br>";
-		echo "<span style='background-color:$color'>$pega_order[id]|$pega_order[coin]|$pega_order[status]|$pega_order[price]</span> </br>";
-
-		if ($pega_order['coin'] == 'TRX' or $pega_order['coin'] == 'USDT_TRC20') {
-			echo "<a href='https://tronscan.org/#/transaction/$pega_order[hash]'>LINK</a>";
-		}
-		if ($pega_order['coin'] == 'ETH' or $pega_order['coin'] == 'USDT_ERC20') {
-			echo "<a href='https://etherscan.io/tx/$pega_order[hash]'>LINK</a>";
-		}
-		if ($pega_order['coin'] == 'BTC') {
-			echo "<a href='https://blockchair.com/bitcoin/transaction/$pega_order[hash]'>LINK</a>";
-		}
-
-		echo "</br>CRYPTO PRICE: $pega_order[price_crypto] </br>
-		  CRYPTO PRICE PAID: $pega_order[price_crypto_payed] </br>
-		  
-		  WALLET: $pega_order[wallet]</br>
-		  WITHDRAWAL: $pega_order[withdrawn] </br>";
-
-		if ($pega_order['withdrawn'] == 1) {
-			$change = "Zero";
-		}
-		if ($pega_order['withdrawn'] == 0) {
-			$change = "One";
-		}
-
-		echo "<a href='?change_withdrawal=1&id=$pega_order[id]'>CHANGE WITHDRAWAL TO $change</a></br>";
-		echo "--------------------------------------------------------------------------</br>";
-		echo "--------------------------------------------------------------------------</br>";
-
-	}
-
+	// while ($pega_order = mysqli_fetch_array($sql)) {
+	
+	// 	if ($pega_order['status'] == "failed") {
+	// 		$color = 'red';
+	// 	}
+	// 	if ($pega_order['status'] == "Expired") {
+	// 		$color = 'orange';
+	// 	}
+	// 	if ($pega_order['status'] == "Paid") {
+	// 		$color = 'green';
+	// 	}
+	// 	if ($pega_order['status'] == "Pending") {
+	// 		$color = 'yellow';
+	// 	}
+	
+	// 	echo "--------------------------------------------------------------------------</br>";
+	// 	echo "--------------------------------------------------------------------------</br>";
+	// 	echo "<span style='background-color:$color'>$pega_order[id]|$pega_order[coin]|$pega_order[status]|$pega_order[price]</span> </br>";
+	
+	// 	if ($pega_order['coin'] == 'TRX' or $pega_order['coin'] == 'USDT_TRC20') {
+	// 		echo "<a href='https://tronscan.org/#/transaction/$pega_order[hash]'>LINK</a>";
+	// 	}
+	// 	if ($pega_order['coin'] == 'ETH' or $pega_order['coin'] == 'USDT_ERC20') {
+	// 		echo "<a href='https://etherscan.io/tx/$pega_order[hash]'>LINK</a>";
+	// 	}
+	// 	if ($pega_order['coin'] == 'BTC') {
+	// 		echo "<a href='https://blockchair.com/bitcoin/transaction/$pega_order[hash]'>LINK</a>";
+	// 	}
+	
+	// 	echo "</br>CRYPTO PRICE: $pega_order[price_crypto] </br>
+	// 	  CRYPTO PRICE PAID: $pega_order[price_crypto_payed] </br>
+	
+	// 	  WALLET: $pega_order[wallet]</br>
+	// 	  WITHDRAWAL: $pega_order[withdrawn] </br>";
+	
+	// 	if ($pega_order['withdrawn'] == 1) {
+	// 		$change = "Zero";
+	// 	}
+	// 	if ($pega_order['withdrawn'] == 0) {
+	// 		$change = "One";
+	// 	}
+	
+	// 	echo "<a href='?change_withdrawal=1&id=$pega_order[id]'>CHANGE WITHDRAWAL TO $change</a></br>";
+	// 	echo "--------------------------------------------------------------------------</br>";
+	// 	echo "--------------------------------------------------------------------------</br>";
+	
+	// }
+	
 	?>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
