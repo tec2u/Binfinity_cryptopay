72512664d2fbce0def9c43724cf45752e2b14647
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-03-25 17:06:23 -0300

    add img trc20

--- app/Http/Controllers/ApiApp.php
+++ app/Http/Controllers/ApiApp.php
@@ -491,7 +491,7 @@ class ApiApp extends Controller
                 $logo = 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=029';
             }
             if ($nodeOrderSave->coin == 'USDT_TRC20') {
-                $logo = asset('images/tron-usdt.png');
+                $logo = 'https://crypto.binfinitybank.com/public/images/tron-usdt.png';
             }
             if ($nodeOrderSave->coin == 'USDT_ERC20') {
                 $logo = 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=029';
