876f69edba0ee926e2fefc44974b549f33056aa4
Gabriel Almeida <gabriel.almeiidda@gmail.com>
2024-03-27 15:32:56 -0300

    .

--- resources/views/wallets/list.blade.php
+++ resources/views/wallets/list.blade.php
@@ -198,11 +198,11 @@
                   </tbody>
                 </table> --}}
 
-                <div class="container-wallet">
+                {{-- <div class="container-wallet">
                   <div class="wallet-header"></div>
                   <div class="wallet-main"></div>
                   <div class="wallet-footer"></div>
-                </div>
+                </div> --}}
 
                 @foreach ($wallets as $chave => $valor)
                   @if (isset($icons[$chave]))
