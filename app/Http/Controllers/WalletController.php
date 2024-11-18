9a6c1d565ab40bbffcb4896a99bdfbe3ab531deb
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2024-11-18 15:18:45 -0300

    debug

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -224,7 +224,6 @@ class WalletController extends Controller
                 //     $wallet->save();
                 //     // dd($wallet);
                 // }
-break;
                 // $wallets = Wallet::where('user_id', $user->id)->where('coin', $request->coin)->get();
             }
             // dd($wallets);
